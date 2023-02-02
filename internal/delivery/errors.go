f0f4e51e2a65ce268b95cd3a9f7c36920c5546c4
Aleksei Rusinov <a.rusinov@basis.center>
2023-02-02 17:19:37 +0200

    Http errors

--- internal/delivery/errors.go
+++ internal/delivery/errors.go
@@ -1,10 +1,20 @@
 package delivery
 
-import "errors"
-
 var (
-	ErrBadParams        = errors.New("bad params width/height")
-	ErrBadParsedAddress = errors.New("incorrect url for source file")
-	ErrReadImage        = errors.New("reading image error")
-	ErrImageProcessing  = errors.New("image processing is not possible")
+	ErrBadParams        = NewError("bad params width/height")
+	ErrBadParsedAddress = NewError("incorrect url for source file")
+	ErrReadImage        = NewError("reading image error")
+	ErrImageProcessing  = NewError("image processing is not possible")
 )
+
+type Error struct {
+	msg string
+}
+
+func NewError(msg string) *Error {
+	return &Error{msg: msg}
+}
+
+func (err *Error) Error() string {
+	return err.msg
+}
